9f66288694bd4f886f8f77cf5805acc5fb38762a
Douglas Tertuliano <douglasjtds@gmail.com>
2022-12-13 17:31:57 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Douglas Tertuliano on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
